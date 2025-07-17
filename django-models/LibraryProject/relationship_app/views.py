27eef2ea494fbffb27d521531f9e46684f18024a
gerald-nyerer <geraldnyerer100@gmail.com>
2025-07-17 12:20:58 +0300

    implementing user authetication in relationship app.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
+
 # Create your views here.
 def book_list(request):
     books = Book.objects.all()  
@@ -19,3 +22,13 @@ class BookDetailView(DetailView):
         context['book'] = self.object.books.all() 
         return context
     
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Optional: log in after registration
+            return redirect('list_all_books')  # Redirect to any view
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
