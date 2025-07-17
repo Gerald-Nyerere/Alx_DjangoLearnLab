9f487457a4ab3631a87715a91233f121ba487f41
gerald-nyerer <geraldnyerer100@gmail.com>
2025-07-17 11:25:14 +0300

    implementing relationship in the relationship app.

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,7 +2,8 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 def query_books_by_author(author_name):
     print(f"\nBooks by author: {author_name}")
-    books = Book.objects.filter(author__name=author_name)
+    author = Author.objects.get(name=author_name)
+    books = Book.objects.filter(author=author)
     for book in books:
         print(f"- {book.title}")
 
