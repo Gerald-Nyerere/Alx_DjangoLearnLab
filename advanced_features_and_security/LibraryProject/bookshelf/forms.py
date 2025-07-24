6f6c0aed8b4043edb5e03c8e646a64c881076c30
gerald-nyerer <geraldnyerer100@gmail.com>
2025-07-24 13:44:27 +0300

    Implementing Security Best Practices in Django

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -4,4 +4,8 @@ from .models import Book
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ['title', 'author']
+        fields = ['title', 'author', 'publication_year ']
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(label='Your Name', max_length=100)
+    email = forms.EmailField(label='Your Email')
