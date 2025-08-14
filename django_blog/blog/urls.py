e34aca287d0bd5b9ac8dbd8a74f507936d6c0915
gerald-nyerer <geraldnyerer100@gmail.com>
2025-08-14 12:50:38 +0300

    Implementing the Blog's User Authentication System

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -10,9 +10,8 @@ urlpatterns = [
 
     
     # Blog Post CRUD operations
-    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+   
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
-    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-    path('', views.PostListView.as_view(), name='home'), 
+   
 ]
