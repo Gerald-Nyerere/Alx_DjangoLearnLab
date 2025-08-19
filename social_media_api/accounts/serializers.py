2cf9e96fddf79670443c02e82a51e82e976cc450
gerald-nyerer <geraldnyerer100@gmail.com>
2025-08-19 14:29:26 +0300

    Initial setup and project configuration for django blog.

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,28 +1,23 @@
 from rest_framework import serializers
-from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
 
-    email = serializers.CharField(required=True) 
-    password = serializers.CharField(required=True)
     class Meta:
         model = User
-        fields = ['email', 'password']
+        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        user = get_user_model().objects.create_user(
+        user = User.objects.create_user(
+            username=validated_data['username'],
             email=validated_data['email'],
-            password=validated_data['password']
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
         )
         Token.objects.create(user=user)
         return user
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['email']
-
