2a97e82cceb171665163f73d5272a97c9c33b035
gerald-nyerer <geraldnyerer100@gmail.com>
2025-08-19 14:14:34 +0300

    Initial setup and project configuration for django blog.

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,9 +5,9 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    email = serializers.CharField(required=True)
-    password = serializers.CharField(write_only=True, required=True)
 
+    email = serializers.CharField(required=True) 
+    password = serializers.CharField(required=True)
     class Meta:
         model = User
         fields = ['email', 'password']
@@ -26,7 +26,3 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['email']
 
-
-class UserLoginSerializer(serializers.Serializer):
-    email = serializers.CharField(required=True)
-    password = serializers.CharField(write_only=True, required=True)
