ff0bef082dc4a828ba938a984165c814b7902884
gerald-nyerer <geraldnyerer100@gmail.com>
2025-08-19 13:03:45 +0300

    Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,23 +1,35 @@
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
+    token = serializers.CharField(read_only=True)
 
     class Meta:
         model = User
-        fields = ['email', 'password']
+        fields = ['email', 'password', 'confirm_password', 'token']
+
+    def validate(self, data):
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError("Passwords do not match")
+        return data
 
     def create(self, validated_data):
+        validated_data.pop('confirm_password')
         user = User.objects.create_user(
-            email=validated_data.get('email'),
+            email=validated_data['email'],
             password=validated_data['password']
         )
+        token, created = Token.objects.get_or_create(user=user)
+        user.token = token.key
         return user
     
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['email', 'password']
+
 
