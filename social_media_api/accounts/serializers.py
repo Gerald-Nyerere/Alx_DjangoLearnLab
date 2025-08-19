2cbbb44d56e15c3bc40f6760728c08166205eeb8
gerald-nyerer <geraldnyerer100@gmail.com>
2025-08-19 13:12:46 +0300

    Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -3,41 +3,26 @@ from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-    confirm_password = serializers.CharField(write_only=True)
-    token = serializers.CharField(read_only=True)
 
     class Meta:
         model = User
-        fields = ['email', 'password', 'confirm_password', 'token']
-
-    def validate(self, data):
-        if data['password'] != data['confirm_password']:
-            raise serializers.ValidationError("Passwords do not match")
-        return data
+        fields = ['email', 'password']
 
     def create(self, validated_data):
-        validated_data.pop('confirm_password')
-        user = User.objects.create_user(
-            email=validated_data['email'],
+        user = get_user_model().objects.create_user(
+            email=validated_data.get('email'),
             password=validated_data['password']
         )
-        token, created = Token.objects.get_or_create(user=user)
-        user.token = token.key
+        
+        Token.objects.create(user=user)
         return user
     
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password']
-        extra_kwargs = {
-            'password': {'write_only': True}
-        }
-
-    def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
+        fields = ['email', 'password']
 
 
+"", "serializers.CharField()", "Token.objects.create", "get_user_model().objects.create_user
