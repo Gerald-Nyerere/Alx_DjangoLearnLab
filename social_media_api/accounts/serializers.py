3d590b63fbeef690b91cffb2ae436db57de2a9eb
gerald-nyerer <geraldnyerer100@gmail.com>
2025-08-19 13:18:59 +0300

    Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -22,5 +22,5 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['email', 'password']
+        fields = ['email']
 
