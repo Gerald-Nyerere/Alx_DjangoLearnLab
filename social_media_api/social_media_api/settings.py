8ace536b6469b86780e66c933ef362b188d53d0c
gerald-nyerer <geraldnyerer100@gmail.com>
2025-08-21 22:08:58 +0300

    Implementing Notifications and Likes Functionality

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -22,11 +22,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-%frn4hd@8z)84)exh1xag6aes&mvb3)%g%pxxe8m5fd*lr8dqg'
 
+# Security settings
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_SSL_REDIRECT = True 
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
+# Static & media
+STATIC_ROOT = BASE_DIR / "staticfiles"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Application definition
 
